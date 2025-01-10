09702eee669f6e44c6804d5c3f48a8c5c617924c
Sam <szacharopoulos@uniphar.ie>
2025-01-10 13:21:59 +0000

    Fix token acquition

--- src/Uniphar.Dotnet.Cli/Commands/GetTokenCommand.cs
+++ src/Uniphar.Dotnet.Cli/Commands/GetTokenCommand.cs
@@ -13,20 +13,35 @@ internal sealed class GetTokenCommand : AsyncCommand<GetTokenCommand.Settings>
         [CommandOption("-s|--scope")]
         [DefaultValue(".default")]
         public string? Scope { get; set; }
+
+        [CommandOption("--useCIAM")]
+        [DefaultValue("false")]
+        public bool? UseCIAM { get; set; }
     }
 
 
     public async override Task<int> ExecuteAsync(CommandContext context, Settings settings)
     {
-        var app = PublicClientApplicationBuilder.Create(settings.ClientId.ToString())
-                                               .WithTenantId(settings.TenantId.ToString())
-                                               .WithDefaultRedirectUri()
-                                               .Build();
+        var authorityUri = (settings.UseCIAM ?? false) ? "https://{0}.ciamlogin.com" : "https://login.microsoftonline.com/{0}";
+        var tenantId = settings.TenantId.ToString();
+        var clientId = settings.ClientId.ToString();
+
+        var app = PublicClientApplicationBuilder.Create(clientId)
+                                                .WithTenantId(tenantId)
+                                                .WithAuthority(string.Format(authorityUri, tenantId))
+                                                .WithDefaultRedirectUri()
+                                                .Build();
         
         var scope = string.IsNullOrWhiteSpace(settings.Scope) ? ".default" : settings.Scope;
 
-        var result = await app.AcquireTokenInteractive([$"{settings.ClientId}/{scope}"]).ExecuteAsync();
-        
+        AnsiConsole.MarkupLine($"[bold][steelblue1]- TenantId    : [/][/][italic]{tenantId}[/]");
+        AnsiConsole.MarkupLine($"[bold][steelblue1]- ClientId    : [/][/][italic]{clientId}[/]");
+        AnsiConsole.MarkupLine($"[bold][steelblue1]- AuthorityId : [/][/][italic]{authorityUri}[/]");
+        AnsiConsole.MarkupLine($"[bold][steelblue1]- Scope       : [/][/][italic]{scope}[/]");
+
+
+        var result = await app.AcquireTokenInteractive([$"{clientId}/{scope}"]).ExecuteAsync();
+
         AnsiConsole.MarkupLine($"{Environment.NewLine}[purple]{result.AccessToken}[/]");
 
         return 0;
