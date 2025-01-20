4f8cff50e28c2c23e414befd9d729a4e8b654a88
Sam Z. <codapus-co@users.noreply.github.com>
2025-01-20 13:34:35 +0000

    Fix issues with scope and output (break lines were being added) (#5)

--- src/Uniphar.Dotnet.Cli/Commands/GetTokenCommand.cs
+++ src/Uniphar.Dotnet.Cli/Commands/GetTokenCommand.cs
@@ -11,25 +11,51 @@ internal sealed class GetTokenCommand : AsyncCommand<GetTokenCommand.Settings>
         public required Guid ClientId { get; set; }
 
         [CommandOption("-s|--scope")]
-        [DefaultValue(".default")]
+        [DefaultValue(null)]
         public string? Scope { get; set; }
+
+        [CommandOption("--authority")]
+        [DefaultValue("ms")]
+        public string? Authority { get; set; }
     }
 
 
     public async override Task<int> ExecuteAsync(CommandContext context, Settings settings)
     {
-        var app = PublicClientApplicationBuilder.Create(settings.ClientId.ToString())
-                                               .WithTenantId(settings.TenantId.ToString())
-                                               .WithDefaultRedirectUri()
-                                               .Build();
-        
-        var scope = string.IsNullOrWhiteSpace(settings.Scope) ? ".default" : settings.Scope;
+        var tenantId = settings.TenantId.ToString();
+        var clientId = settings.ClientId.ToString();
+        var authority = settings.Authority ?? "ms";
 
-        var result = await app.AcquireTokenInteractive([$"{settings.ClientId}/{scope}"]).ExecuteAsync();
-        
-        AnsiConsole.MarkupLine($"{Environment.NewLine}[purple]{result.AccessToken}[/]");
+        var (authorityUri, isPredefinedAuthority) = authority.ToLower() switch
+        {
+            "ms" => ($"https://login.microsoftonline.com/{tenantId}", true),
+            "ciam" => ($"https://{tenantId}.ciamlogin.com", true),
+            _ when Uri.TryCreate(authority, UriKind.Absolute, out _) => (authority, false),
+            _ => throw new Exception("Invalid authority URI")
+        };
+
+        if (!isPredefinedAuthority)
+            AnsiConsole.MarkupLine("[bold yellow]Warning:[/] Using an arbitrary URI for authority is temporary. Please submit a PR to add your authority (currently supported: ms, ciam).");
+
+        var app = PublicClientApplicationBuilder.Create(clientId)
+                                                .WithTenantId(tenantId)
+                                                .WithAuthority(authorityUri)
+                                                .WithDefaultRedirectUri()
+                                                .Build();
+
+        var scope = string.IsNullOrWhiteSpace(settings.Scope) ? $"{clientId}/.default" : settings.Scope;
+
+        AnsiConsole.MarkupLine($"[bold][steelblue1]- TenantId    : [/][/][italic]{tenantId}[/]");
+        AnsiConsole.MarkupLine($"[bold][steelblue1]- ClientId    : [/][/][italic]{clientId}[/]");
+        AnsiConsole.MarkupLine($"[bold][steelblue1]- AuthorityId : [/][/][italic]{authorityUri}[/]");
+        AnsiConsole.MarkupLine($"[bold][steelblue1]- Scope       : [/][/][italic]{scope}[/]");
+        AnsiConsole.WriteLine();
+
+        var result = await app.AcquireTokenInteractive([scope]).ExecuteAsync();
+
+        using (ConsoleEx.SetColors(foreground: ConsoleColor.DarkGreen))
+            Console.WriteLine(result.AccessToken); // We are using Console because AnsiConsole is adding line breaks to the output
 
         return 0;
     }
 }
-
---
+++ src/Uniphar.Dotnet.Cli/Extensions/ConsoleEx.cs
@@ -0,0 +1,28 @@
+﻿public static class ConsoleEx
+{
+    public static ConsoleColorReset SetColors(ConsoleColor? foreground = default, ConsoleColor? background = default)
+    {
+        return new ConsoleColorReset(foreground, background);
+    }
+
+    public readonly struct ConsoleColorReset : IDisposable
+    {
+        private ConsoleColor Foreground { get; }
+        private ConsoleColor Background { get; }
+
+        public ConsoleColorReset(ConsoleColor? newForeground = default, ConsoleColor? newBackground = default)
+        {
+            Foreground = Console.ForegroundColor;
+            Background = Console.BackgroundColor;
+
+            Console.ForegroundColor = newForeground ?? Foreground;
+            Console.BackgroundColor = newBackground ?? Background;
+        }
+
+        public void Dispose()
+        {
+            Console.ForegroundColor = Foreground;
+            Console.BackgroundColor = Background;
+        }
+    }
+}
